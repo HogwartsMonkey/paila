4ba139696392bf201c71886de553f7bdc5c85612
HogwartsMonkey <lirongavrielevy@gmail.com>
2019-04-07 17:29:23 +0300

    some changes to style

--- src/text-reveal.js
+++ src/text-reveal.js
@@ -10,12 +10,12 @@ export class FadeHeader extends React.Component {
       <div className="headline-container">
       
         <div className="h-1">
-          <Fade when={this.props.willReavel}  delay={300}  >
+          <Fade when={this.props.willReavel}  duration={1000}   delay={300}  >
             <h1>Paila</h1>
           </Fade>
         </div>
         <div className="h-1">
-          <Fade when={this.props.willReavel}  delay={300}  >
+          <Fade when={this.props.willReavel} duration={1000}    delay={300}  >
             <h1>Creating Value.</h1>
           </Fade>
         </div>
@@ -34,15 +34,15 @@ export class TextHeader extends React.Component{
       return(
           <div className="text">
 
-            <Fade when={this.props.willReavel} delay={300}>
-            <p>Crunching Data.</p>
+            <Fade when={this.props.willReavel}  duration={1000}  delay={300}>
+            <p>Exploring Data.</p>
             </Fade> 
 
-            <Fade when={this.props.willReavel} delay={300}>
+            <Fade when={this.props.willReavel} duration={1000}   delay={300}>
           <p>Serving Creativity.</p>
           </Fade>
 
-            <Fade when={this.props.willReavel} delay={300} cascade>
+            <Fade when={this.props.willReavel} duration={1000}  delay={300} cascade>
           <p>Delivering.</p>
           </Fade>
 
@@ -58,7 +58,7 @@ export class NavBar extends React.Component{
   render(){
   
 return(
-    <Fade when={this.props.willReavel} delay={300}>
+    <Fade when={this.props.willReavel} duration={1000}  delay={300}>
       <div className="nav_menu">
         <ul>
             <li>Who</li>
